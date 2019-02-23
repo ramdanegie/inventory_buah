02d36f3f343322cdf9ca0b832d940ddc09021f39
Egie Ramdan <ramdanegie@gmail.com>
2019-02-23 13:27:51 +0700

    Map Produk OTW

--- front-ng/src/app/page/modules/master/map-produk-to-satuan-standar/map-produk-to-satuan-standar.component.ts
+++ front-ng/src/app/page/modules/master/map-produk-to-satuan-standar/map-produk-to-satuan-standar.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '../../../../helper/service/HttpClient';
+import { DataHandler } from '../../../../helper/handler/DataHandler';
+import { TableHandler } from '../../../../helper/handler/TableHandler';
+import { Observable } from 'rxjs/Rx';
+import { LazyLoadEvent, Message, ConfirmDialogModule, ConfirmationService, SelectItem } from 'primeng/primeng';
+import { AlertService, InfoService, Configuration, LoaderService } from '../../../../helper';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-map-produk-to-satuan-standar',
   templateUrl: './map-produk-to-satuan-standar.component.html',
-  styleUrls: ['./map-produk-to-satuan-standar.component.scss']
+  styleUrls: ['./map-produk-to-satuan-standar.component.scss'],
+  providers: [ConfirmationService]
 })
 export class MapProdukToSatuanStandarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private alertService: AlertService,
+    private InfoService: InfoService,
+    private httpService: HttpClient,
+    private confirmationService: ConfirmationService,
+    private dataHandler: DataHandler,
+    private fb: FormBuilder,
+    private loader: LoaderService
+  ) { }
+
 
   ngOnInit() {
   }
