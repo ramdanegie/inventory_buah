0ccc03bed9c6815d184eb5b9c3e9f6222686f6ef
ramdanegie <ramdanegie@gmail.com>
2019-09-01 03:30:46 +0800

    setting

--- front-ng/src/app/helper/config.ts
+++ front-ng/src/app/helper/config.ts
@@ -1,9 +1,23 @@
 export class Configuration {
   static get() {
-    return {
-      apiBackend: 'http://localhost:8000/service/',
-      headerToken: 'token',
-      authLogin: ':8000',
+    if (window.location.hostname.indexOf('110.137') > -1) {
+      return {
+        apiBackend: 'http://110.137.212.85:8000/service/',
+        headerToken: 'token',
+        authLogin: ':8000',
+      }
+    } if (window.location.hostname.indexOf('localhost') > -1) {
+      return {
+        apiBackend: 'http://localhost:8000/service/',
+        headerToken: 'token',
+        authLogin: ':8000',
+      }
+    }else{
+      return {
+        apiBackend: 'http://192.168.1.5:8000/service/',
+        headerToken: 'token',
+        authLogin: ':8000',
+      }
     }
   }
 }
